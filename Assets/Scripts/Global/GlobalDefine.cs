bb4f3c98e0217cd45429c651bf8e13c70f707344
Nevill <>
2020-08-04 08:51:22 +0900

    구조 수정 파일 추가

---
+++ Assets/Scripts/Global/GlobalDefine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// 포톤 통신 타입 정의
+/// </summary>
+public enum E_PTDefine
+{
+    PT_NONE,
+    PT_READY,
+    PT_CHANGELAYER,
+    PT_DEACTIVEWAIT,
+    PT_ADDSP,
+    PT_SETDECK,
+    PT_SPAWNMINION,
+    PT_SPAWN,
+    PT_MAX,
+}
