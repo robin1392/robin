0b09893a8f7aa7b1fd47cb37095916bb8f2ae530
Nevill <nevill@111percent.net>
2020-08-13 09:14:45 +0900

    game data dice 변경

--- Assets/Scripts/JSONData/DiceInfo.cs
+++ Assets/Scripts/JSONData/DiceInfo.cs
@@ -9,15 +9,27 @@ public class DiceInfoData
 	public int grade = 0;
 	public int castType = 0;
 	public int moveType = 0;
-	public string rscName = "";
+	public int targetMoveType = 0;
+	public string prefabName = "";
 	public int spawnMultiply = 0;
 	public string iconName = "";
 	public string cardName = "";
+	public int colorR = 0;
+	public int colorG = 0;
+	public int colorB = 0;
 	public float power = 0.0f;
-	public float powerByUpgrade = 0.0f;
-	public float powerGameUp = 0.0f;
+	public float powerUpgrade = 0.0f;
+	public float powerInGameUp = 0.0f;
 	public float maxHealth = 0.0f;
-	public List<int> testArray = new List<int>();
+	public float maxHpUpgrade = 0.0f;
+	public float maxHpInGameUp = 0.0f;
+	public float effect = 0.0f;
+	public float effectUpgrade = 0.0f;
+	public float effectInGameUp = 0.0f;
+	public float attackSpeed = 0.0f;
+	public float moveSpeed = 0.0f;
+	public float range = 0.0f;
+	public float searchRange = 0.0f;
 
 }
 
@@ -55,15 +67,27 @@ public class DiceInfo
 			info.grade = (int)JsonDataParse.GetParseData(info.grade.GetType(), jsonData[i]["grade"].ToString());
 			info.castType = (int)JsonDataParse.GetParseData(info.castType.GetType(), jsonData[i]["castType"].ToString());
 			info.moveType = (int)JsonDataParse.GetParseData(info.moveType.GetType(), jsonData[i]["moveType"].ToString());
-			info.rscName = (string)JsonDataParse.GetParseData(info.rscName.GetType(), jsonData[i]["rscName"].ToString());
+			info.targetMoveType = (int)JsonDataParse.GetParseData(info.targetMoveType.GetType(), jsonData[i]["targetMoveType"].ToString());
+			info.prefabName = (string)JsonDataParse.GetParseData(info.prefabName.GetType(), jsonData[i]["prefabName"].ToString());
 			info.spawnMultiply = (int)JsonDataParse.GetParseData(info.spawnMultiply.GetType(), jsonData[i]["spawnMultiply"].ToString());
 			info.iconName = (string)JsonDataParse.GetParseData(info.iconName.GetType(), jsonData[i]["iconName"].ToString());
 			info.cardName = (string)JsonDataParse.GetParseData(info.cardName.GetType(), jsonData[i]["cardName"].ToString());
+			info.colorR = (int)JsonDataParse.GetParseData(info.colorR.GetType(), jsonData[i]["colorR"].ToString());
+			info.colorG = (int)JsonDataParse.GetParseData(info.colorG.GetType(), jsonData[i]["colorG"].ToString());
+			info.colorB = (int)JsonDataParse.GetParseData(info.colorB.GetType(), jsonData[i]["colorB"].ToString());
 			info.power = (float)JsonDataParse.GetParseData(info.power.GetType(), jsonData[i]["power"].ToString());
-			info.powerByUpgrade = (float)JsonDataParse.GetParseData(info.powerByUpgrade.GetType(), jsonData[i]["powerByUpgrade"].ToString());
-			info.powerGameUp = (float)JsonDataParse.GetParseData(info.powerGameUp.GetType(), jsonData[i]["powerGameUp"].ToString());
+			info.powerUpgrade = (float)JsonDataParse.GetParseData(info.powerUpgrade.GetType(), jsonData[i]["powerUpgrade"].ToString());
+			info.powerInGameUp = (float)JsonDataParse.GetParseData(info.powerInGameUp.GetType(), jsonData[i]["powerInGameUp"].ToString());
 			info.maxHealth = (float)JsonDataParse.GetParseData(info.maxHealth.GetType(), jsonData[i]["maxHealth"].ToString());
-			info.testArray = (List<int>)JsonDataParse.GetParseData(info.testArray.GetType(), jsonData[i]["testArray"].ToString());
+			info.maxHpUpgrade = (float)JsonDataParse.GetParseData(info.maxHpUpgrade.GetType(), jsonData[i]["maxHpUpgrade"].ToString());
+			info.maxHpInGameUp = (float)JsonDataParse.GetParseData(info.maxHpInGameUp.GetType(), jsonData[i]["maxHpInGameUp"].ToString());
+			info.effect = (float)JsonDataParse.GetParseData(info.effect.GetType(), jsonData[i]["effect"].ToString());
+			info.effectUpgrade = (float)JsonDataParse.GetParseData(info.effectUpgrade.GetType(), jsonData[i]["effectUpgrade"].ToString());
+			info.effectInGameUp = (float)JsonDataParse.GetParseData(info.effectInGameUp.GetType(), jsonData[i]["effectInGameUp"].ToString());
+			info.attackSpeed = (float)JsonDataParse.GetParseData(info.attackSpeed.GetType(), jsonData[i]["attackSpeed"].ToString());
+			info.moveSpeed = (float)JsonDataParse.GetParseData(info.moveSpeed.GetType(), jsonData[i]["moveSpeed"].ToString());
+			info.range = (float)JsonDataParse.GetParseData(info.range.GetType(), jsonData[i]["range"].ToString());
+			info.searchRange = (float)JsonDataParse.GetParseData(info.searchRange.GetType(), jsonData[i]["searchRange"].ToString());
 
             
             dicData.Add(info.id , info);
