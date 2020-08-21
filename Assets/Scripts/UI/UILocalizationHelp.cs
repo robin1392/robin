cb024ba9f6faaac3244608be6ef26552ed1984ee
Nevill <nevill@111percent.net>
2020-08-21 17:06:13 +0900

    ui blur 추가

---
+++ Assets/Scripts/UI/UILocalizationHelp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILocalizationHelp : MonoBehaviour
+{
+
+    [Header("Localization Info")] 
+    public int localizeLangKey;
+
+
+    #region unity base
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    #endregion
+    
+    #region convert lang
+
+    public void ConvertLang()
+    {
+        Text textThis = this.transform.GetComponent<Text>();
+        
+    }
+    #endregion
+    
+    
+}
---
+++ Assets/Scripts/UI/UI_OutGame/UI_PopupOption.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ED;
+
+
+public class UI_PopupOption : UI_Popup
+{
+    
+    
+    #region override
+    
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+            
+    }
+    
+    
+    public override void Close()
+    {
+        
+    }
+    
+    #endregion
+
+
+
+    #region option system
+
+    
+
+    #endregion
+
+
+    #region click event
+
+    
+
+    #endregion
+}
