2c84461ee7c7ce2e1ca24b262773fa9cf34ae657
ED <ed@111percent.net>
2020-09-29 09:36:05 +0900

    덱 수정시 인디케이터 생성

--- Assets/Scripts/UI/UI_OutGame/UI_FadeIn.cs
+++ Assets/Scripts/UI/UI_OutGame/UI_FadeIn.cs
@@ -9,17 +9,26 @@ using DG.Tweening;
 public class UI_FadeIn : MonoBehaviour
 {
     public Image image_Blind;
-    public float fadeinTime = 2f;
+    public float fadeTime = 2f;
+    public bool isFadeOut;
 
-    private void Start()
+    private void OnEnable()
     {
         if (image_Blind != null)
         {
             image_Blind.enabled = true;
-            image_Blind.DOFade(0f, fadeinTime).SetEase(Ease.InCirc).OnComplete(() =>
+            var c = image_Blind.color;
+            c.a = isFadeOut ? 0 : 1f;
+            image_Blind.color = c;
+            image_Blind.DOFade(isFadeOut ? 0.8f : 0f, fadeTime).SetEase(Ease.InCirc).OnComplete(() =>
             {
-                image_Blind.enabled = false;
+                if (isFadeOut == false) image_Blind.enabled = false;
             });
         }
+    }
+
+    private void OnDisable()
+    {
+        if (image_Blind) image_Blind.enabled = false;
     }
 }
