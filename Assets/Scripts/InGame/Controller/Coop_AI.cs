8491943f4ecf7a47862fbd316007649aaa3e6a0b
ED <ed@111percent.net>
2020-11-17 10:31:46 +0900

    coop ai minion balance

--- Assets/Scripts/InGame/Controller/Coop_AI.cs
+++ Assets/Scripts/InGame/Controller/Coop_AI.cs
@@ -192,8 +192,8 @@ namespace ED
 
                 m.targetMoveType = DICE_MOVE_TYPE.ALL;
                 m.ChangeLayer(false);
-                m.power = Mathf.Clamp(100f + 5f * InGameManager.Get().wave, 0, 300f);
-                m.maxHealth = Mathf.Clamp(300f + 20f * InGameManager.Get().wave, 0, 1500f);
+                m.power = Mathf.Clamp(100f + 10f * InGameManager.Get().wave, 0, 300f);
+                m.maxHealth = Mathf.Clamp(300f + 50f * InGameManager.Get().wave, 0, 2000f);
                 m.attackSpeed = 1f;
                 m.moveSpeed = 1f;
                 m.eyeLevel = 1;
