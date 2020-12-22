f66203d01573dfe65a37a5db5805e8183aa85e3e
ED <ed@111percent.net>
2020-12-22 11:03:33 +0900

    modify guardian1

---
+++ Assets/Scripts/InGame/Guardian/Guardian_01.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ED;
+
+public class Guardian_01 : Minion
+{
+    public override void Attack()
+    {
+        if (target == null || target.isAlive == false || IsTargetInnerRange() == false) return;
+            
+        if( InGameManager.IsNetwork && (isMine || controller.isPlayingAI) )
+        {
+            base.Attack();
+            controller.MinionAniTrigger(id, "Attack", target.id);
+        }
+        else if(InGameManager.IsNetwork == false)
+        {
+            base.Attack();
+            animator.SetTrigger(_animatorHashAttack);
+        }
+    }
+}
