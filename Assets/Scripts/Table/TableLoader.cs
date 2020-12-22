5e52a063e3a2172a60058a5cafeb7d842a01e644
nsoh <hf98066@gmail.com>
2020-12-22 16:09:06 +0900

    modify table - down from bucket

--- Assets/Scripts/Table/TableLoader.cs
+++ Assets/Scripts/Table/TableLoader.cs
@@ -7,16 +7,16 @@ namespace Table
 {
     public interface ITableLoader<K, V>
     {
-        bool Run(string path, ref Dictionary<K, V> outValues);
+        bool Run(string sourcePath, string fileName, string targetPath, ref Dictionary<K, V> outValues);
     }
 
 
     public class TableLoaderLocalCSV<K, V> : ITableLoader<K, V>
         where V : ITableData<K>, new()
     {
-        public bool Run(string path, ref Dictionary<K, V> outValues)
+        public bool Run(string sourcePath, string fileName, string targetPath, ref Dictionary<K, V> outValues)
         {
-            using (var reader = new StreamReader(path))
+            using (var reader = new StreamReader(sourcePath + "/" + fileName))
             {
                 int row = 0;
                 List<int> exceptIndex = new List<int>();
@@ -67,58 +67,78 @@ namespace Table
     public class TableLoaderRemoteCSV<K, V> : ITableLoader<K, V>
         where V : ITableData<K>, new()
     {
-        public bool Run(string path, ref Dictionary<K, V> outValues)
+        public bool Run(string sourcePath, string fileName, string targetPath, ref Dictionary<K, V> outValues)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(path);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(sourcePath + "/" + fileName);
             request.ContentType = "application/json";
             request.Method = "GET";
 
-            List<int> exceptIndex = new List<int>();
             var response = (HttpWebResponse)request.GetResponse();
-            using (var reader = new StreamReader(response.GetResponseStream()))
+            var resStream = response.GetResponseStream();
+
+            StreamReader reader;
+            if (targetPath.Length != 0)
             {
-                int row = 0;
-                while (!reader.EndOfStream)
+                if (Directory.Exists(targetPath) == false)
                 {
-                    var line = reader.ReadLine();
-                    List<string> cols = new List<string>(line.Split(','));
-                    if (row == 0)
-                    {
-                        for (int i = cols.Count - 1; i >= 0; i--)
-                        {
-                            if (cols[i].StartsWith("~") == true)
-                            {
-                                exceptIndex.Add(i);
-                            }
-                        }
-                    }
-                    
-                    if (row++ < 2)
-                    {
-                        continue;
-                    }
-
-                    foreach (var index in exceptIndex)
-                    {
-                        cols.RemoveAt(index);
-                    }
-
-
-                    V col = new V();
-                    col.Serialize(cols.ToArray());
-
-                    K key = col.PK();
-                    if (outValues.ContainsKey(key) == true)
-                    {
-                        return false;
-                    }
-
-                    outValues.Add(key, col);
+                    Directory.CreateDirectory(targetPath);
                 }
+
+                using (var fileStream = new FileStream(targetPath + "/" + fileName, FileMode.Create, FileAccess.Write))
+                {
+                    resStream.CopyTo(fileStream);
+                }
+
+                reader = new StreamReader(targetPath + "/" + fileName);
+            }
+            else
+            {
+                reader = new StreamReader(resStream);
             }
 
+
+            List<int> exceptIndex = new List<int>();
+            int row = 0;
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                List<string> cols = new List<string>(line.Split(','));
+                if (row == 0)
+                {
+                    for (int i = cols.Count - 1; i >= 0; i--)
+                    {
+                        if (cols[i].StartsWith("~") == true)
+                        {
+                            exceptIndex.Add(i);
+                        }
+                    }
+                }
+
+                if (row++ < 2)
+                {
+                    continue;
+                }
+
+                foreach (var index in exceptIndex)
+                {
+                    cols.RemoveAt(index);
+                }
+
+
+                V col = new V();
+                col.Serialize(cols.ToArray());
+
+                K key = col.PK();
+                if (outValues.ContainsKey(key) == true)
+                {
+                    return false;
+                }
+
+                outValues.Add(key, col);
+            }
+
+            reader.Close();
             return true;
         }
     }
-
 }
