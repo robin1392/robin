921fecae25b47bf67630d770974080f7a1b48c0e
nsoh <hf98066@gmail.com>
2020-12-23 14:33:38 +0900

    modify GetRankCallback - comment error code

--- Assets/Scripts/UI/UI_OutGame/UI_Popup_Rank.cs
+++ Assets/Scripts/UI/UI_OutGame/UI_Popup_Rank.cs
@@ -26,17 +26,17 @@ public class UI_Popup_Rank : UI_Popup
         StopAllCoroutines();
         UI_Main.Get().obj_IndicatorPopup.SetActive(false);
 
-        for (int i = 0; i < arrSlot.Length; i++)
-        {
-            if (msg.RankInfo.Length > i)
-            {
-                arrSlot[i].Initialize(i + 1, msg.RankInfo[i].Trophy, msg.RankInfo[i].UserName);
-            }
-            else
-            {
-                arrSlot[i].Initialize(i + 1, -1, string.Empty);
-            }
-        }
+        //for (int i = 0; i < arrSlot.Length; i++)
+        //{
+        //    if (msg.RankInfo.Length > i)
+        //    {
+        //        arrSlot[i].Initialize(i + 1, msg.RankInfo[i].Trophy, msg.RankInfo[i].UserName);
+        //    }
+        //    else
+        //    {
+        //        arrSlot[i].Initialize(i + 1, -1, string.Empty);
+        //    }
+        //}
     }
 
     IEnumerator WaitCoroutine()
