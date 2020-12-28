4c1e0d9052f9c2c9f774a72cf8755291d88d6eef
nsoh <hf98066@gmail.com>
2020-12-28 17:16:34 +0900

    modify table - rankingreward.cs

--- Assets/Scripts/Table/Data/TDataRankingReward.cs
+++ Assets/Scripts/Table/Data/TDataRankingReward.cs
@@ -10,6 +10,7 @@ namespace Table.Data
     {
         public int id { get; set; }
         public string name { get; set; }
+        public int rankRewardType { get; set; }
         public int rankMin { get; set; }
         public int rankMax { get; set; }
         public int rankRewardItem01 { get; set; }
@@ -36,20 +37,21 @@ namespace Table.Data
         {
             id = int.Parse(cols[0]);
             name = cols[1];
-            rankMin = int.Parse(cols[2]);
-            rankMax = int.Parse(cols[3]);
-            rankRewardItem01 = int.Parse(cols[4]);
-            rankRewardIValue01 = int.Parse(cols[5]);
-            rankRewardItem02 = int.Parse(cols[6]);
-            rankRewardIValue02 = int.Parse(cols[7]);
-            rankRewardItem03 = int.Parse(cols[8]);
-            rankRewardIValue03 = int.Parse(cols[9]);
-            rankRewardItem04 = int.Parse(cols[10]);
-            rankRewardIValue04 = int.Parse(cols[11]);
-            rankRewardItem05 = int.Parse(cols[12]);
-            rankRewardIValue05 = int.Parse(cols[13]);
-            rankRestartPoint = int.Parse(cols[14]);
-            rankRangeGroup = int.Parse(cols[15]);
+            rankRewardType = int.Parse(cols[2]);
+            rankMin = int.Parse(cols[3]);
+            rankMax = int.Parse(cols[4]);
+            rankRewardItem01 = int.Parse(cols[5]);
+            rankRewardIValue01 = int.Parse(cols[6]);
+            rankRewardItem02 = int.Parse(cols[7]);
+            rankRewardIValue02 = int.Parse(cols[8]);
+            rankRewardItem03 = int.Parse(cols[9]);
+            rankRewardIValue03 = int.Parse(cols[10]);
+            rankRewardItem04 = int.Parse(cols[11]);
+            rankRewardIValue04 = int.Parse(cols[12]);
+            rankRewardItem05 = int.Parse(cols[13]);
+            rankRewardIValue05 = int.Parse(cols[14]);
+            rankRestartPoint = int.Parse(cols[15]);
+            rankRangeGroup = int.Parse(cols[16]);
         }
     }
 }
