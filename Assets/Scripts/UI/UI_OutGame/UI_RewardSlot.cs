6c6bd9c0425a9a7ebc9d12d4646e72b756d70ffb
ED <ed@111percent.net>
2021-01-04 15:48:38 +0900

    seasonpass and class reward get button link

--- Assets/Scripts/UI/UI_OutGame/UI_RewardSlot.cs
+++ Assets/Scripts/UI/UI_OutGame/UI_RewardSlot.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using RandomWarsProtocol;
+using RandomWarsProtocol.Msg;
 using RandomWarsResource.Data;
 using UnityEngine;
 using UnityEngine.UI;
@@ -20,8 +22,13 @@ public class UI_RewardSlot : MonoBehaviour
     public GameObject[] arrObj_Lock;
     public GameObject[] arrObj_Check;
 
+    private int row;
+    private TDataSeasonpassReward dataPremium;
+    private TDataSeasonpassReward dataNormal;
+
     public void Initialize(int row, int myTrophy)
     {
+        this.row = row;
         float minTrophy = 0;
         float maxTrophy = 0;
         
@@ -48,19 +55,19 @@ public class UI_RewardSlot : MonoBehaviour
         }
         else
         {
-            var dataPass = new TDataSeasonpassReward();
-            var dataNormal = new TDataSeasonpassReward();
+            dataPremium = new TDataSeasonpassReward();
+            dataNormal = new TDataSeasonpassReward();
             TableManager.Get().SeasonpassReward.GetData(row, out dataNormal);
-            TableManager.Get().SeasonpassReward.GetData(row + 1000, out dataPass);
+            TableManager.Get().SeasonpassReward.GetData(row + 1000, out dataPremium);
 
-            text_Trophy.text = dataPass.trophyPoint.ToString();
-            arrText_Value[0].text = $"{dataPass.rewardItem}\nx{dataPass.rewardItemValue}";
+            text_Trophy.text = dataPremium.trophyPoint.ToString();
+            arrText_Value[0].text = $"{dataPremium.rewardItem}\nx{dataPremium.rewardItemValue}";
             arrText_Value[1].text = $"{dataNormal.rewardItem}\nx{dataNormal.rewardItemValue}";
             
             // set min, max
             if (TableManager.Get().SeasonpassReward.GetData(row - 1, out dataNormal))
             {
-                minTrophy = Mathf.Lerp(dataNormal.trophyPoint, dataPass.trophyPoint, 0.5f);
+                minTrophy = Mathf.Lerp(dataNormal.trophyPoint, dataPremium.trophyPoint, 0.5f);
             }
             
             if (TableManager.Get().SeasonpassReward.GetData(row + 1, out dataNormal) == false)
@@ -70,37 +77,62 @@ public class UI_RewardSlot : MonoBehaviour
                 image_Guage.rectTransform.sizeDelta = v;
                 image_Guage_BG.rectTransform.sizeDelta = v;
 
-                maxTrophy = dataPass.trophyPoint;
+                maxTrophy = dataPremium.trophyPoint;
             }
             else
             {
-                maxTrophy = Mathf.Lerp(dataPass.trophyPoint, dataNormal.trophyPoint, 0.5f);
+                maxTrophy = Mathf.Lerp(dataPremium.trophyPoint, dataNormal.trophyPoint, 0.5f);
             }
             
             // set buttons
-            if (myTrophy < dataPass.trophyPoint)    // 트로피 부족
-            {
-                arrObj_Lock[0].SetActive(true);
-                arrObj_Lock[1].SetActive(true);
-                arrObj_Check[0].SetActive(false);
-                arrObj_Check[1].SetActive(false);
-                arrButton[0].interactable = false;
-                arrButton[1].interactable = false;
-            }
-            else
-            {
-                bool getNormal = UserInfoManager.Get().GetUserInfo().seasonPassRewardIds.Contains(row);
-                bool getPass = UserInfoManager.Get().GetUserInfo().seasonPassRewardIds.Contains(row + 1000);
-                
-                arrObj_Lock[0].SetActive(false);
-                arrObj_Lock[1].SetActive(false);
-                arrObj_Check[0].SetActive(getPass);
-                arrObj_Check[1].SetActive(getNormal);
-                arrButton[0].interactable = !getPass;
-                arrButton[1].interactable = !getNormal;
-            }
+            SetButton();
         }
 
         image_Guage.fillAmount = (myTrophy - minTrophy) / (float)maxTrophy;
+    }
+
+    public void SetButton()
+    {
+        if (UserInfoManager.Get().GetUserInfo().seasonTrophy < dataPremium.trophyPoint)    // 트로피 부족
+        {
+            arrObj_Lock[0].SetActive(true);
+            arrObj_Lock[1].SetActive(true);
+            arrObj_Check[0].SetActive(false);
+            arrObj_Check[1].SetActive(false);
+            arrButton[0].interactable = false;
+            arrButton[1].interactable = false;
+        }
+        else
+        {
+            bool getNormal = UserInfoManager.Get().GetUserInfo().seasonPassRewardIds.Contains(row);
+            bool getPass = UserInfoManager.Get().GetUserInfo().seasonPassRewardIds.Contains(row + 1000);
+                
+            arrObj_Lock[0].SetActive(false);
+            arrObj_Lock[1].SetActive(false);
+            arrObj_Check[0].SetActive(getPass);
+            arrObj_Check[1].SetActive(getNormal);
+            arrButton[0].interactable = !getPass;
+            arrButton[1].interactable = !getNormal;
+        }
+    }
+
+    public void Click_PremiumGet()
+    {
+        NetworkManager.Get().GetSeasonPassRewardReq(UserInfoManager.Get().GetUserInfo().userID, row + 1000, GetCallback);
+    }
+
+    public void Click_NormalGet()
+    {
+        NetworkManager.Get().GetSeasonPassRewardReq(UserInfoManager.Get().GetUserInfo().userID, row, GetCallback);
+    }
+
+    public void GetCallback(MsgGetSeasonPassRewardAck msg)
+    {
+        if (msg.ErrorCode == GameErrorCode.SUCCESS)
+        {
+            
+        }
+        
+        SetButton();
     }
 }
--- Assets/Scripts/UI/UI_OutGame/UI_TrophyRewardSlot.cs
+++ Assets/Scripts/UI/UI_OutGame/UI_TrophyRewardSlot.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using RandomWarsProtocol;
+using RandomWarsProtocol.Msg;
 using RandomWarsResource.Data;
 using UnityEngine;
 using UnityEngine.UI;
@@ -20,8 +22,17 @@ public class UI_TrophyRewardSlot : MonoBehaviour
     public GameObject[] arrObj_Lock;
     public GameObject[] arrObj_Check;
 
+    private int row;
+    private int getVipRow;
+    private int getNormalRow;
+    private TDataClassReward dataPremium;
+    private TDataClassReward dataNormal;
+
     public void Initialize(int row, int myTrophy, int getVipRow, int getNormalRow)
     {
+        this.row = row;
+        this.getVipRow = getVipRow;
+        this.getNormalRow = getNormalRow;
         float minTrophy = 0;
         float maxTrophy = 0;
         
@@ -48,19 +59,19 @@ public class UI_TrophyRewardSlot : MonoBehaviour
         }
         else
         {
-            var dataPass = new TDataClassReward();
-            var dataNormal = new TDataClassReward();
+            dataPremium = new TDataClassReward();
+            dataNormal = new TDataClassReward();
             TableManager.Get().ClassReward.GetData(row, out dataNormal);
-            TableManager.Get().ClassReward.GetData(row + 1000, out dataPass);
+            TableManager.Get().ClassReward.GetData(row + 1000, out dataPremium);
 
-            text_Trophy.text = dataPass.rankPoint.ToString();
-            arrText_Value[0].text = $"{dataPass.ItemId}\nx{dataPass.ItemValue}";
+            text_Trophy.text = dataPremium.rankPoint.ToString();
+            arrText_Value[0].text = $"{dataPremium.ItemId}\nx{dataPremium.ItemValue}";
             arrText_Value[1].text = $"{dataNormal.ItemId}\nx{dataNormal.ItemValue}";
             
             // set min, max
             if (TableManager.Get().ClassReward.GetData(row - 1, out dataNormal))
             {
-                minTrophy = Mathf.Lerp(dataNormal.rankPoint, dataPass.rankPoint, 0.5f);
+                minTrophy = Mathf.Lerp(dataNormal.rankPoint, dataPremium.rankPoint, 0.5f);
             }
             
             if (TableManager.Get().ClassReward.GetData(row + 1, out dataNormal) == false)
@@ -70,37 +81,62 @@ public class UI_TrophyRewardSlot : MonoBehaviour
                 image_Guage.rectTransform.sizeDelta = v;
                 image_Guage_BG.rectTransform.sizeDelta = v;
 
-                maxTrophy = dataPass.rankPoint;
+                maxTrophy = dataPremium.rankPoint;
             }
             else
             {
-                maxTrophy = Mathf.Lerp(dataPass.rankPoint, dataNormal.rankPoint, 0.5f);
+                maxTrophy = Mathf.Lerp(dataPremium.rankPoint, dataNormal.rankPoint, 0.5f);
             }
             
             // set buttons
-            if (myTrophy < dataPass.rankPoint)    // 트로피 부족
-            {
-                arrObj_Lock[0].SetActive(true);
-                arrObj_Lock[1].SetActive(true);
-                arrObj_Check[0].SetActive(false);
-                arrObj_Check[1].SetActive(false);
-                arrButton[0].interactable = false;
-                arrButton[1].interactable = false;
-            }
-            else
-            {
-                bool getNormal = getNormalRow >= row;
-                bool getPass = getVipRow >= row + 1000;
-                
-                arrObj_Lock[0].SetActive(false);
-                arrObj_Lock[1].SetActive(false);
-                arrObj_Check[0].SetActive(getPass);
-                arrObj_Check[1].SetActive(getNormal);
-                arrButton[0].interactable = !getPass;
-                arrButton[1].interactable = !getNormal;
-            }
+            SetButton();
         }
 
         image_Guage.fillAmount = (myTrophy - minTrophy) / (float)maxTrophy;
+    }
+
+    public void SetButton()
+    {
+        if (UserInfoManager.Get().GetUserInfo().trophy < dataPremium.rankPoint)    // 트로피 부족
+        {
+            arrObj_Lock[0].SetActive(true);
+            arrObj_Lock[1].SetActive(true);
+            arrObj_Check[0].SetActive(false);
+            arrObj_Check[1].SetActive(false);
+            arrButton[0].interactable = false;
+            arrButton[1].interactable = false;
+        }
+        else
+        {
+            bool getNormal = getNormalRow >= row;
+            bool getPass = getVipRow >= row + 1000;
+                
+            arrObj_Lock[0].SetActive(false);
+            arrObj_Lock[1].SetActive(false);
+            arrObj_Check[0].SetActive(getPass);
+            arrObj_Check[1].SetActive(getNormal);
+            arrButton[0].interactable = !getPass;
+            arrButton[1].interactable = !getNormal;
+        }
+    }
+    
+    public void Click_PremiumGet()
+    {
+        NetworkManager.Get().GetClassRewardReq(UserInfoManager.Get().GetUserInfo().userID, row + 1000, GetCallback);
+    }
+
+    public void Click_NormalGet()
+    {
+        NetworkManager.Get().GetClassRewardReq(UserInfoManager.Get().GetUserInfo().userID, row, GetCallback);
+    }
+    
+    public void GetCallback(MsgGetClassRewardAck msg)
+    {
+        if (msg.ErrorCode == GameErrorCode.SUCCESS)
+        {
+            
+        }
+        
+        SetButton();
     }
 }
