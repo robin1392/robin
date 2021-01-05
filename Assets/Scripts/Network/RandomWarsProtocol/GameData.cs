b46ff6364a54421cf57dbaa6069524a7911e071e
nsoh <hf98066@gmail.com>
2021-01-05 12:29:56 +0900

    modify protocol - MsgQuestInfo

--- Assets/Scripts/Network/RandomWarsProtocol/GameData.cs
+++ Assets/Scripts/Network/RandomWarsProtocol/GameData.cs
@@ -1900,9 +1900,9 @@ namespace RandomWarsProtocol
         public int RemainResetTime;
         // 퀘스트 데이터
         public MsgQuestData[] QuestData;
-        // 일일 보상 아이디
-        public int DayRewardId;
         // 일일 보상 획득 상태
-        public bool DayRewardState;
+        public bool GetDayRewardState;
+        // 일일 보상 획득까지 남은 시간(초단위)
+        public int RemainDayRewardTime;
     }
 }
