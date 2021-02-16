1ad680a1150cf4f0c41638db226d02fbc1987793
kwazii-111percent <kawzii@111percent.net>
2021-02-16 17:31:29 +0900

    ParrelSync 적용 https://www.notion.so/e0081064a43e48aea03da68204f782ba

---
+++ Assets/Editor/CloneProjectConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using ParrelSync;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class CloneProjectConfiguration
+{
+    static CloneProjectConfiguration()
+    {
+        if (!ClonesManager.IsClone())
+        {
+            return;
+        }
+            
+        var arg = ClonesManager.GetArgument();
+        if (PlayerSettings.productName.Contains(arg))
+        {
+            return;
+        }
+
+        PlayerSettings.productName += $"_{arg}";
+    }
+}
