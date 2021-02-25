0de4ffb41561e7cc534bea4242397e1bcb94c2e2
kwazii-111percent <kawzii@111percent.net>
2021-02-25 15:25:28 +0900

    스타트씬 자동 로드 기능을 프로젝트 세팅에서 설정할 수 있도록 수정

--- Assets/Editor/StartScene.cs
+++ Assets/Editor/StartScene.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 
 [InitializeOnLoadAttribute]
-public class StartScene : EditorWindow
+public static class StartScene
 {
+    public const string DisableStartScenePrefsKey = "AutoStartScenePrefsKey";
+
     static StartScene()
     {
         EditorApplication.playModeStateChanged += OnPlayModeChanged;
@@ -16,21 +16,50 @@ public class StartScene : EditorWindow
 
     private static void OnPlayModeChanged(PlayModeStateChange state)
     {
-        return;
-        switch(state)
+        if (EditorPrefs.GetBool(DisableStartScenePrefsKey, false)) return;
+
+        switch (state)
         {
-        case PlayModeStateChange.EnteredEditMode:
-            EditorSceneManager.OpenScene(File.ReadAllText(".lastScene"));
-            break;
-        case PlayModeStateChange.ExitingEditMode:
-            File.WriteAllText(".lastScene", EditorSceneManager.GetActiveScene().path);
-            if(EditorSceneManager.GetActiveScene().isDirty)
-            {
-                EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
-            }
-            EditorSceneManager.OpenScene("Assets/01.Scenes/StartScene.unity");
-            EditorApplication.isPlaying = true;
-            break;
+            case PlayModeStateChange.EnteredEditMode:
+                EditorSceneManager.OpenScene(File.ReadAllText(".lastScene"));
+                break;
+            case PlayModeStateChange.ExitingEditMode:
+                File.WriteAllText(".lastScene", SceneManager.GetActiveScene().path);
+                if (SceneManager.GetActiveScene().isDirty) EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+
+                EditorSceneManager.OpenScene("Assets/01.Scenes/StartScene.unity");
+                EditorApplication.isPlaying = true;
+                break;
         }
+    }
+}
+
+internal static class StartSceneSettingsIMGUIRegister
+{
+    [SettingsProvider]
+    public static SettingsProvider CreateMyCustomSettingsProvider()
+    {
+        // First parameter is the path in the Settings window.
+        // Second parameter is the scope of this setting: it only appears in the Project Settings window.
+        var provider = new SettingsProvider("Project/RandomWars", SettingsScope.Project)
+        {
+            // By default the last token of the path is used as display name if no label is provided.
+            label = "RandomWars",
+            // Create the SettingsProvider and initialize its drawing (IMGUI) function in place:
+            guiHandler = searchContext =>
+            {
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    var toggleValue = EditorGUILayout.ToggleLeft("StartScene 자동 로드 비활성화",
+                        EditorPrefs.GetBool(StartScene.DisableStartScenePrefsKey));
+                    EditorPrefs.SetBool(StartScene.DisableStartScenePrefsKey, toggleValue);
+                }
+            },
+
+            // Populate the search keywords to enable smart search filtering and label highlighting:
+            keywords = new HashSet<string>(new[] {"StartScene"})
+        };
+
+        return provider;
     }
 }
