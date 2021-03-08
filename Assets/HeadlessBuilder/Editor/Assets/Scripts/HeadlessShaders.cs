cb003be4ec6901f210af877d2422035a2b95c85c
kwazii-111percent <kawzii@111percent.net>
2021-03-08 11:10:59 +0900

    HeadlessBuilder 추가

---
+++ Assets/HeadlessBuilder/Editor/Assets/Scripts/HeadlessShaders.cs
@@ -0,0 +1,43 @@
+﻿/* 
+ * Headless Builder
+ * (c) Salty Devs, 2020
+ * 
+ * Please do not publish or pirate this code.
+ * We worked really hard to make it.
+ * 
+ */
+
+
+#if UNITY_2018_2_OR_NEWER
+
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+class HeadlessShader : IPreprocessShaders
+{
+
+    public int callbackOrder { get { return 1024; } }
+
+    public void OnProcessShader(
+        Shader shader, ShaderSnippetData snippet, IList<ShaderCompilerData> shaderCompilerData)
+    {
+        if (!Headless.IsBuildingHeadless() || !HeadlessBuilder.ShouldStripShaders())
+            return;
+
+        int shaderCount = shaderCompilerData.Count;
+
+        for (int i = 0; i < shaderCompilerData.Count; ++i)
+        {
+            shaderCompilerData.RemoveAt(i);
+            --i;
+        }
+
+        HeadlessBuilder.shaderCount += shaderCount;
+    }
+}
+
+#endif
