eaf656183f69a5287cc2c45b0032d1a88e3fed84
nsoh <hf98066@gmail.com>
2021-03-10 10:51:24 +0900

    edit mailbox info

--- Assets/GameBase/Template/MailBox/GameBaseMailBox/Common/GameBaeMailBoxData.cs
+++ Assets/GameBase/Template/MailBox/GameBaseMailBox/Common/GameBaeMailBoxData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Service.Core;
 
 namespace Template.MailBox.GameBaseMailBox.Common
 {
@@ -9,24 +10,13 @@ namespace Template.MailBox.GameBaseMailBox.Common
     {
         // 아이디 (식별값)
         public string mailId;
-        // 제목
-        public Dictionary<string, string> mailTitles;
-        // 내용
-        public Dictionary<string, string> mailContents;
-        // 발송자
-        public string mailFrom;
+        // 테이블 아이디
+        public int mailTableId;
         // 상품 목록
-        public MailItemInfo[] mailItems;
+        public ItemBaseInfo[] mailItems;
         // 만료까지 남은 시간
         public int expireRemainTime;
-    }
-
-    // 우편 상품 정보
-    [Serializable]
-    public class MailItemInfo
-    {
-        public int itemType;
-        public int itemId;
-        public int itemAmount;
+        // 추가 텍스트 목록
+        public List<string> listText;
     }
 }
