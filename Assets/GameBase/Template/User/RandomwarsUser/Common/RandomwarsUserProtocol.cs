51a785be272759e5df736701bff902feeaae1a17
nsoh <hf98066@gmail.com>
2021-03-15 16:51:15 +0900

    add addreward protocol

--- Assets/GameBase/Template/User/RandomwarsUser/Common/RandomwarsUserProtocol.cs
+++ Assets/GameBase/Template/User/RandomwarsUser/Common/RandomwarsUserProtocol.cs
@@ -14,11 +14,20 @@ namespace Template.User.RandomwarsUser.Common
         UserInfoReq,
         UserInfoAck,
 
-        UserRewardReq,
-        UserRewardAck,
+        UserTrophyRewardReq,
+        UserTrophyRewardAck,
+
+        UserAdRewardReq,
+        UserAdRewardAck,
 
         UserTutorialEndReq,
         UserTutorialEndAck,
+
+        UserNameInitReq,
+        UserNameInitAck,
+
+        UserNameChangeReq,
+        UserNameChangeAck,
 
 
         End,
@@ -33,10 +42,14 @@ namespace Template.User.RandomwarsUser.Common
             {
                 {(int)ERandomwarsProtocol.UserInfoReq, ReceiveUserInfoReq},
                 {(int)ERandomwarsProtocol.UserInfoAck, ReceiveUserInfoAck},
-                {(int)ERandomwarsProtocol.UserRewardReq, ReceiveUserRewardReq},
-                {(int)ERandomwarsProtocol.UserRewardAck, ReceiveUserRewardAck},
+                {(int)ERandomwarsProtocol.UserTrophyRewardReq, ReceiveUserTrophyRewardReq},
+                {(int)ERandomwarsProtocol.UserTrophyRewardAck, ReceiveUserTrophyRewardAck},
                 {(int)ERandomwarsProtocol.UserTutorialEndReq, ReceiveUserTutorialEndReq},
-                {(int)ERandomwarsProtocol.UserTutorialEndAck, ReceiveUserTutorialEndAck},           };
+                {(int)ERandomwarsProtocol.UserNameInitReq, ReceiveUserNameInitReq},
+                {(int)ERandomwarsProtocol.UserNameInitAck, ReceiveUserNameInitAck},
+                {(int)ERandomwarsProtocol.UserNameChangeReq, ReceiveUserNameChangeReq},
+                {(int)ERandomwarsProtocol.UserNameChangeAck, ReceiveUserNameChangeAck},
+            };
         }
 
 
@@ -93,32 +106,32 @@ namespace Template.User.RandomwarsUser.Common
         #endregion    
 
 
-        #region UserReward ---------------------------------------------------------------------
-        public bool UserRewardReq(ISender sender, int rewardId, int targetType, ReceiveUserRewardAckDelegate callback)
+        #region UserTrophyReward ---------------------------------------------------------------------
+        public bool UserTrophyRewardReq(ISender sender, int rewardId, int targetType, ReceiveUserTrophyRewardAckDelegate callback)
         {
-            ReceiveUserRewardAckHandler = callback;
+            ReceiveUserTrophyRewardAckHandler = callback;
             JObject json = new JObject();
             json.Add("accessToken", sender.GetAccessToken());
             json.Add("rewardId", rewardId);
             json.Add("targetType", targetType);
-            return sender.SendHttpPost((int)ERandomwarsProtocol.UserRewardReq, "userreward", json.ToString());
+            return sender.SendHttpPost((int)ERandomwarsProtocol.UserTrophyRewardReq, "usertrophyreward", json.ToString());
         }
 
 
-        public delegate (ERandomwarsUserErrorCode errorCode, int[] arrayRewardId, ItemBaseInfo[] arrayRewardInfo, QuestData[] arrayQuestData) ReceiveUserRewardReqDelegate(string accessToken, int rewardId, int targetType);
-        public ReceiveUserRewardReqDelegate ReceiveUserRewardReqHandler;
-        public bool ReceiveUserRewardReq(ISender sender, byte[] msg, int length)
+        public delegate (ERandomwarsUserErrorCode errorCode, int[] arrayRewardId, ItemBaseInfo[] arrayRewardInfo, QuestData[] arrayQuestData) ReceiveUserTrophyRewardReqDelegate(string accessToken, int rewardId, int targetType);
+        public ReceiveUserTrophyRewardReqDelegate ReceiveUserTrophyRewardReqHandler;
+        public bool ReceiveUserTrophyRewardReq(ISender sender, byte[] msg, int length)
         {
             string json = Encoding.Default.GetString(msg, 0, length);
             JObject jObject = JObject.Parse(json);
             string accessToken = (string)jObject["accessToken"];
             int rewardId = (int)jObject["rewardId"];
             int targetType = (int)jObject["targetType"];
-            var res = ReceiveUserRewardReqHandler(accessToken, rewardId, targetType);
-            return UserRewardAck(sender, res.errorCode, res.arrayRewardId, res.arrayRewardInfo, res.arrayQuestData);
+            var res = ReceiveUserTrophyRewardReqHandler(accessToken, rewardId, targetType);
+            return UserTrophyRewardAck(sender, res.errorCode, res.arrayRewardId, res.arrayRewardInfo, res.arrayQuestData);
         }
 
-        public bool UserRewardAck(ISender sender, ERandomwarsUserErrorCode errorCode, int[] arrayRewardId, ItemBaseInfo[] arrayRewardInfo, QuestData[] arrayQuestData)
+        public bool UserTrophyRewardAck(ISender sender, ERandomwarsUserErrorCode errorCode, int[] arrayRewardId, ItemBaseInfo[] arrayRewardInfo, QuestData[] arrayQuestData)
         {
             JObject json = new JObject();
             json.Add("errorCode", (int)errorCode);
@@ -129,9 +142,9 @@ namespace Template.User.RandomwarsUser.Common
         }
 
 
-        public delegate bool ReceiveUserRewardAckDelegate(ERandomwarsUserErrorCode errorCode, int[] arrayRewardId, ItemBaseInfo[] arrayRewardInfo, QuestData[] arrayQuestData);
-        public ReceiveUserRewardAckDelegate ReceiveUserRewardAckHandler;
-        public bool ReceiveUserRewardAck(ISender sender, byte[] msg, int length)
+        public delegate bool ReceiveUserTrophyRewardAckDelegate(ERandomwarsUserErrorCode errorCode, int[] arrayRewardId, ItemBaseInfo[] arrayRewardInfo, QuestData[] arrayQuestData);
+        public ReceiveUserTrophyRewardAckDelegate ReceiveUserTrophyRewardAckHandler;
+        public bool ReceiveUserTrophyRewardAck(ISender sender, byte[] msg, int length)
         {
             string json = Encoding.Default.GetString(msg, 0, length);
             JObject jObject = JObject.Parse(json);
@@ -139,9 +152,56 @@ namespace Template.User.RandomwarsUser.Common
             int[] arrayRewardId = JsonConvert.DeserializeObject<int[]>(jObject["arrayRewardId"].ToString());
             ItemBaseInfo[] arrayRewardInfo = JsonConvert.DeserializeObject<ItemBaseInfo[]>(jObject["arrayRewardInfo"].ToString());
             QuestData[] arrayQuestData = JsonConvert.DeserializeObject<QuestData[]>(jObject["arrayQuestData"].ToString());
-            return ReceiveUserRewardAckHandler(errorCode, arrayRewardId, arrayRewardInfo, arrayQuestData);
+            return ReceiveUserTrophyRewardAckHandler(errorCode, arrayRewardId, arrayRewardInfo, arrayQuestData);
         }
-        #endregion                      
+        #endregion                 
+
+
+        #region UserAdReward ---------------------------------------------------------------------
+        public bool UserAdRewardReq(ISender sender, string rewardId, ReceiveUserAdRewardAckDelegate callback)
+        {
+            ReceiveUserAdRewardAckHandler = callback;
+            JObject json = new JObject();
+            json.Add("accessToken", sender.GetAccessToken());
+            json.Add("rewardId", rewardId);
+            return sender.SendHttpPost((int)ERandomwarsProtocol.UserAdRewardReq, "useradreward", json.ToString());
+        }
+
+
+        public delegate (ERandomwarsUserErrorCode errorCode, ItemBaseInfo[] arrayRewardInfo, QuestData[] arrayQuestData) ReceiveUserAdRewardReqDelegate(string accessToken, string rewardId);
+        public ReceiveUserAdRewardReqDelegate ReceiveUserAdRewardReqHandler;
+        public bool ReceiveUserAdRewardReq(ISender sender, byte[] msg, int length)
+        {
+            string json = Encoding.Default.GetString(msg, 0, length);
+            JObject jObject = JObject.Parse(json);
+            string accessToken = (string)jObject["accessToken"];
+            string rewardId = (string)jObject["rewardId"];
+            var res = ReceiveUserAdRewardReqHandler(accessToken, rewardId);
+            return UserAdRewardAck(sender, res.errorCode, res.arrayRewardInfo, res.arrayQuestData);
+        }
+
+        public bool UserAdRewardAck(ISender sender, ERandomwarsUserErrorCode errorCode, ItemBaseInfo[] arrayRewardInfo, QuestData[] arrayQuestData)
+        {
+            JObject json = new JObject();
+            json.Add("errorCode", (int)errorCode);
+            json.Add("arrayRewardInfo", JsonConvert.SerializeObject(arrayRewardInfo));
+            json.Add("arrayQuestData", JsonConvert.SerializeObject(arrayQuestData));
+            return sender.SendHttpResult(json.ToString());
+        }
+
+
+        public delegate bool ReceiveUserAdRewardAckDelegate(ERandomwarsUserErrorCode errorCode, ItemBaseInfo[] arrayRewardInfo, QuestData[] arrayQuestData);
+        public ReceiveUserAdRewardAckDelegate ReceiveUserAdRewardAckHandler;
+        public bool ReceiveUserAdRewardAck(ISender sender, byte[] msg, int length)
+        {
+            string json = Encoding.Default.GetString(msg, 0, length);
+            JObject jObject = JObject.Parse(json);
+            ERandomwarsUserErrorCode errorCode = (ERandomwarsUserErrorCode)(int)jObject["errorCode"];
+            ItemBaseInfo[] arrayRewardInfo = JsonConvert.DeserializeObject<ItemBaseInfo[]>(jObject["arrayRewardInfo"].ToString());
+            QuestData[] arrayQuestData = JsonConvert.DeserializeObject<QuestData[]>(jObject["arrayQuestData"].ToString());
+            return ReceiveUserAdRewardAckHandler(errorCode, arrayRewardInfo, arrayQuestData);
+        }
+        #endregion         
 
         #region UserTutorialEnd ---------------------------------------------------------------------
         public bool UserTutorialEndReq(ISender sender, ReceiveUserTutorialEndAckDelegate callback)
@@ -184,5 +244,92 @@ namespace Template.User.RandomwarsUser.Common
             return ReceiveUserTutorialEndAckHandler(errorCode, endTutorial);
         }
         #endregion    
+        #region UserNameInit ---------------------------------------------------------------------
+        public bool UserNameInitReq(ISender sender, string userName, ReceiveUserNameInitAckDelegate callback)
+        {
+            ReceiveUserNameInitAckHandler = callback;
+            JObject json = new JObject();
+            json.Add("accessToken", sender.GetAccessToken());
+            json.Add("userName", userName);
+            return sender.SendHttpPost((int)ERandomwarsProtocol.UserNameInitReq, "usernameinit", json.ToString());
+        }
+
+        public delegate (ERandomwarsUserErrorCode errorCode, string userName, bool isNameInit) ReceiveUserNameInitReqDelegate(string accessToken, string userName);
+        public ReceiveUserNameInitReqDelegate ReceiveUserNameInitReqHandler;
+        public bool ReceiveUserNameInitReq(ISender sender, byte[] msg, int length)
+        {
+            string json = Encoding.Default.GetString(msg, 0, length);
+            JObject jObject = JObject.Parse(json);
+            string accessToken = (string)jObject["accessToken"];
+            string userName = (string)jObject["userName"];
+            var res = ReceiveUserNameInitReqHandler(accessToken, userName);
+            return UserNameInitAck(sender, res.errorCode, res.userName, res.isNameInit);
+        }
+
+        public bool UserNameInitAck(ISender sender, ERandomwarsUserErrorCode errorCode, string userName, bool isNameInit)
+        {
+            JObject json = new JObject();
+            json.Add("errorCode", (int)errorCode);
+            json.Add("userName", userName);
+            json.Add("isNameInit", isNameInit);
+            return sender.SendHttpResult(json.ToString());
+        }
+
+        public delegate bool ReceiveUserNameInitAckDelegate(ERandomwarsUserErrorCode errorCode, string userName, bool isNameInit);
+        public ReceiveUserNameInitAckDelegate ReceiveUserNameInitAckHandler;
+        public bool ReceiveUserNameInitAck(ISender sender, byte[] msg, int length)
+        {
+            string json = Encoding.Default.GetString(msg, 0, length);
+            JObject jObject = JObject.Parse(json);
+            ERandomwarsUserErrorCode errorCode = (ERandomwarsUserErrorCode)(int)jObject["errorCode"];
+            string userName = (string)jObject["userName"];
+            bool isNameInit = (bool)jObject["isNameInit"];
+            return ReceiveUserNameInitAckHandler(errorCode, userName, isNameInit);
+        }
+        #endregion
+
+        #region UserNameChange ---------------------------------------------------------------------
+        public bool UserNameChangeReq(ISender sender, string userName, ReceiveUserNameChangeAckDelegate callback)
+        {
+            ReceiveUserNameChangeAckHandler = callback;
+            JObject json = new JObject();
+            json.Add("accessToken", sender.GetAccessToken());
+            json.Add("userName", userName);
+            return sender.SendHttpPost((int)ERandomwarsProtocol.UserNameChangeReq, "usernamechange", json.ToString());
+        }
+
+        public delegate (ERandomwarsUserErrorCode errorCode, string userName, ItemBaseInfo[] arrayDeleteItemInfo) ReceiveUserNameChangeReqDelegate(string accessToken, string userName);
+        public ReceiveUserNameChangeReqDelegate ReceiveUserNameChangeReqHandler;
+        public bool ReceiveUserNameChangeReq(ISender sender, byte[] msg, int length)
+        {
+            string json = Encoding.Default.GetString(msg, 0, length);
+            JObject jObject = JObject.Parse(json);
+            string accessToken = (string)jObject["accessToken"];
+            string userName = (string)jObject["userName"];
+            var res = ReceiveUserNameChangeReqHandler(accessToken, userName);
+            return UserNameChangeAck(sender, res.errorCode, res.userName, res.arrayDeleteItemInfo);
+        }
+
+        public bool UserNameChangeAck(ISender sender, ERandomwarsUserErrorCode errorCode, string userName, ItemBaseInfo[] arrayDeleteItemInfo)
+        {
+            JObject json = new JObject();
+            json.Add("errorCode", (int)errorCode);
+            json.Add("userName", userName);
+            json.Add("arrayDeleteItemInfo", JsonConvert.SerializeObject(arrayDeleteItemInfo));
+            return sender.SendHttpResult(json.ToString());
+        }
+
+        public delegate bool ReceiveUserNameChangeAckDelegate(ERandomwarsUserErrorCode errorCode, string userName, ItemBaseInfo[] arrayDeleteItemInfo);
+        public ReceiveUserNameChangeAckDelegate ReceiveUserNameChangeAckHandler;
+        public bool ReceiveUserNameChangeAck(ISender sender, byte[] msg, int length)
+        {
+            string json = Encoding.Default.GetString(msg, 0, length);
+            JObject jObject = new JObject();
+            ERandomwarsUserErrorCode errorCode = (ERandomwarsUserErrorCode)(int)jObject["errorCode"];
+            string userName = (string)jObject["userName"];
+            ItemBaseInfo[] arrayItemInfo = JsonConvert.DeserializeObject<ItemBaseInfo[]>(jObject["arrayDeleteItemInfo"].ToString());
+            return ReceiveUserNameChangeAckHandler(errorCode, userName, arrayItemInfo);
+        }
+        #endregion        
     }
 }
