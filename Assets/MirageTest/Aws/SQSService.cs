1b1c8a1e1044238d8e012502814e41274f52a428
kwazii-111percent <kawzii@111percent.net>
2021-03-19 22:31:45 +0900

    SQS 제거

--- Assets/MirageTest/Aws/SQSService.cs
+++ Assets/MirageTest/Aws/SQSService.cs
@@ -1,9 +1,5 @@
 ﻿#if UNITY_EDITOR || UNITY_STANDALONE
 using System.Collections.Generic;
-using Amazon.SecurityToken;
-using Amazon.SecurityToken.Model;
-using Amazon.SQS;
-using Amazon.SQS.Model;
 using Cysharp.Threading.Tasks;
 using Newtonsoft.Json;
 using UnityEngine;
@@ -25,33 +21,40 @@ namespace MirageTest.Aws
 
         public async UniTask SendMessage(List<UserMatchResult> listMatchResult)
         {
-            AssumeRoleRequest roleRequest = new AssumeRoleRequest();
-            roleRequest.RoleArn = _roleArn;
-            roleRequest.RoleSessionName = _roleName;
-            AmazonSecurityTokenServiceClient stsClient = new AmazonSecurityTokenServiceClient();
-            var roleResponse = stsClient.AssumeRoleAsync(roleRequest);
-
-            await roleResponse;
-
-            if (roleResponse.Result == null)
-            {
-                Debug.LogError($"roleResponse 가 널입니다.");
-                return;
-            }
-
-            IAmazonSQS sqs = new AmazonSQSClient(roleResponse.Result.Credentials);
-
-            var getQueueUrlResponse = sqs.GetQueueUrl(_queueName);
-            var myQueueUrl = getQueueUrlResponse.QueueUrl;
-            var sqsMessageRequest = new SendMessageRequest(myQueueUrl, JsonConvert.SerializeObject(listMatchResult));
-            var sendMessageResponse = sqs.SendMessageAsync(sqsMessageRequest);
-            if (sendMessageResponse.Result == null)
-            {
-                Debug.LogError($"sendMessageResponse 가 null입니다.");
-                return;
-            }
-
-            Debug.Log($"메세지 아이디: {sendMessageResponse.Result.MessageId}");
+            // AssumeRoleRequest roleRequest = new AssumeRoleRequest();
+            // roleRequest.RoleArn = _roleArn;
+            // roleRequest.RoleSessionName = _roleName;
+            // AmazonSecurityTokenServiceClient stsClient = new AmazonSecurityTokenServiceClient();
+            // var roleResponse = stsClient.AssumeRoleAsync(roleRequest);
+            //
+            // await roleResponse;
+            //
+            // if (roleResponse.Result == null)
+            // {
+            //     Debug.LogError($"roleResponse 가 널입니다.");
+            //     return;
+            // }
+            //
+            // IAmazonSQS sqs = new AmazonSQSClient(roleResponse.Result.Credentials);
+            //
+            // var getQueueUrlResponse = sqs.GetQueueUrlAsync(_queueName);
+            // await getQueueUrlResponse;
+            // if (getQueueUrlResponse.Result == null)
+            // {
+            //     Debug.LogError($"getQueueUrlResponse 실패");
+            //     return;
+            // }
+            //
+            // var myQueueUrl = getQueueUrlResponse.Result.QueueUrl;
+            // var sqsMessageRequest = new SendMessageRequest(myQueueUrl, JsonConvert.SerializeObject(listMatchResult));
+            // var sendMessageResponse = sqs.SendMessageAsync(sqsMessageRequest);
+            // if (sendMessageResponse.Result == null)
+            // {
+            //     Debug.LogError($"sendMessageResponse 실패");
+            //     return;
+            // }
+            //
+            // Debug.Log($"메세지 아이디: {sendMessageResponse.Result.MessageId}");
         }
     }
 }
