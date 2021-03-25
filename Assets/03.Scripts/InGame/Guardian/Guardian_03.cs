7daaf62df7cbbf8a6468563cca081b644f52a800
ED <ed@111percent.net>
2021-03-25 12:49:15 +0900

    수호자3 수정

--- Assets/03.Scripts/InGame/Guardian/Guardian_03.cs
+++ Assets/03.Scripts/InGame/Guardian/Guardian_03.cs
@@ -5,12 +5,17 @@ using ED;
 
 public class Guardian_03 : Minion
 {
+    public float bulletMoveSpeed = 6f;
     public GameObject pref_Bullet;
+
+    private float _skillCastedTime;
 
     protected override void Start()
     {
         base.Start();
         
+        _animationEvent.event_FireArrow -= FireArrow;
+        _animationEvent.event_FireArrow += FireArrow;
         PoolManager.instance.AddPool(pref_Bullet, 5);
     }
     
@@ -18,20 +23,54 @@ public class Guardian_03 : Minion
     {
         base.Initialize();
 
-        StartCoroutine(SkillCoroutine());
+        _skillCastedTime = -effectCooltime;
     }
     
+    public override IEnumerator Attack()
+    {
+        if (_spawnedTime >= _skillCastedTime + effectCooltime)
+        {
+            _skillCastedTime = _spawnedTime;
+            yield return SkillCoroutine();
+        }
+
+        yield return base.Attack();
+    }
+
+    public void FireArrow()
+    {
+        //TODO: 빼도 되지 않을까? 고민해보자
+        if (target == null || IsTargetInnerRange() == false)
+        {
+            animator.SetTrigger(AnimationHash.Idle);
+            return;
+        }
+
+        if (ActorProxy.isPlayingAI)
+        {
+            ActorProxy.FireBulletWithRelay(E_BulletType.GUARDIAN3_BULLET, target, power, bulletMoveSpeed);
+        }
+    }
+
     IEnumerator SkillCoroutine()
     {
-        while (true)
+        var cols = Physics.OverlapSphere(transform.position, 2f, targetLayer);
+
+        if (cols.Length <= 0) yield break;
+        
+        ActorProxy.PlayAnimationWithRelay(AnimationHash.Skill, target);
+        
+        yield return new WaitForSeconds(0.75f);
+
+        foreach (var col in cols)
         {
-            yield return new WaitForSeconds(effectCooltime);
-            
-            ActorProxy.PlayAnimationWithRelay(AnimationHash.Skill, target);
-
-            yield return new WaitForSeconds(1.716f);
-
-            // Invincibility(2f);
+            var minion = col.GetComponentInParent<Minion>();
+            if (minion != null)
+            {
+                minion.ActorProxy.HitDamage(effect);
+            }
         }
+
+        yield return new WaitForSeconds(0.25f);
     }
 }
