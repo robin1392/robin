3194c231292917a19a0cd0d7a44805d826a8fa7e
kwazii-111percent <kawzii@111percent.net>
2021-03-31 12:57:10 +0900

    [신규주사위] -  방패병 리소스 추가

---
+++ Assets/03.Scripts/InGame/Minions/Minion_Shield.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirage;
+using MirageTest.Scripts;
+using MirageTest.Scripts.SyncAction;
+using UnityEngine;
+
+namespace ED
+{
+    public class Minion_Shield : Minion
+    {
+        [Header("AudioClip")]
+        public AudioClip clip_ShieldMode;
+        
+        private float skillCastedTime;
+        
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            skillCastedTime = -effectCooltime;
+        }
+
+        public void Skill()
+        {
+            if (_spawnedTime >= skillCastedTime + effectCooltime)
+            {
+                skillCastedTime = _spawnedTime;
+                ActorProxy.AddBuff(BuffInfos.HalfDamage, effectDuration);
+
+                var action = new ShielderAction();
+                RunningAction = action;
+                RunLocalAction(action.ActionWithSync(ActorProxy), true);
+            }
+        }
+
+      
+        public override void OnHitDamageOnClient(float damage)
+        {
+            Skill();
+        }
+
+        public override BaseStat SetTarget()
+        {
+            return ActorProxy.GetEnemyTowerOrBoss();
+        }
+    }
+}
