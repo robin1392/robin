e94c04774515fc4f230b0bd2e5403e2cda887df2
kwazii-111percent <kawzii@111percent.net>
2021-04-07 19:43:56 +0900

    도발 구현

--- Assets/03.Scripts/InGame/Minions/Minion_Shield.cs
+++ Assets/03.Scripts/InGame/Minions/Minion_Shield.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Mirage;
 using MirageTest.Scripts;
 using MirageTest.Scripts.SyncAction;
@@ -20,29 +21,75 @@ namespace ED
             base.Initialize();
             skillCastedTime = -effectCooltime;
         }
+        
+        protected override IEnumerator Combat()
+        {
+            while (true)
+            {
+                yield return Skill();
 
-        public void Skill()
+                if (!IsTargetInnerRange())
+                {
+                    ApproachToTarget();
+                }
+                else
+                {
+                    break;
+                }
+
+                yield return null;
+
+                target = SetTarget();
+            }
+
+            StopApproachToTarget();
+
+            if (target == null)
+            {
+                yield break;
+            }
+
+            yield return Attack();
+        }
+
+        public IEnumerator Skill()
         {
             if (_spawnedTime >= skillCastedTime + effectCooltime)
             {
-                skillCastedTime = _spawnedTime;
-                ActorProxy.AddBuff(BuffInfos.HalfDamage, effectDuration);
+                var targets = new List<Minion>();
+                var skillDistance = (ActorProxy as DiceActorProxy).diceInfo.range;
+                var cols = Physics.OverlapSphere(transform.position, skillDistance, targetLayer);
+                foreach (var col in cols)
+                {
+                    var minion = col.GetComponentInParent<Minion>();
+                    if(minion != null && minion.CanBeTarget())
+                    {
+                        targets.Add(minion);
+                    }
+                }
 
+                if (targets.Any() == false)
+                {
+                    yield break;
+                }
+                
+                var duration = 5;//effectDuration;
+                ActorProxy.AddBuff(BuffInfos.HalfDamage, duration);
+
+                foreach (var target in targets)
+                {
+                    target.ActorProxy.AddBuffWithNetId(BuffInfos.Taunted, ActorProxy.NetId, duration);
+                }
+                
                 var action = new ShieldAction();
-                RunningAction = action;
-                RunLocalAction(action.ActionWithSync(ActorProxy), true);
+                yield return action.ActionWithSync(ActorProxy);
+
+                skillCastedTime = _spawnedTime;
             }
         }
 
-      
         public override void OnHitDamageOnClient(float damage)
         {
-            Skill();
-        }
-
-        public override BaseStat SetTarget()
-        {
-            return ActorProxy.GetEnemyTowerOrBossEgg();
         }
     }
 }
